a0397ef1a8c0ccd823ef23af2a2a4303158bd00f
abdelrahman <abdelrahman.magdy.gado@gmail.com>
2022-11-25 10:43:44 +0200

    create endpoint authenication

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import make_response
+from functools import wraps
 from loadjsonfiles import quotes, authors
 from random import choice
 
@@ -15,7 +19,23 @@ def get_quote_author(quote_id):
   return ""
 
 
+def check_token(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    if 'authorization' not in request.headers:
+      return make_response(jsonify({"message": "You are not authorized to use this API!"}), 403)
+
+    token_header = request.headers['authorization']
+    auth_token = token_header.split()[1]
+    if auth_token != app.config["TOKEN"]:
+      return make_response(jsonify({"message": "You are not authorized to use this API!"}), 403)
+    return f(*args, **kwargs)
+  return decorated
+
+
+
 @app.route('/quote/random/', methods=['GET'])
+@check_token
 def get_random_quote():
   random_quote = choice(quotes)
   random_quote_id = random_quote["id"]
