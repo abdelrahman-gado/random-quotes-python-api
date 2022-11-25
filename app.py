7cb677e379da16ec62f023c76808d1e556600d25
abdelrahman <abdelrahman.magdy.gado@gmail.com>
2022-11-25 10:36:08 +0200

    get author name for specific quote id

--- app.py
+++ app.py
@@ -5,9 +5,24 @@ from random import choice
 app = Flask(__name__)
 app.config.from_pyfile("./config.py")
 
+def get_quote_author(quote_id):
+  
+  for author in authors:
+    author_quotes_ids = author["quoteIds"]
+    for id in author_quotes_ids:
+      if (id == quote_id):
+        return author["author"]
+  return ""
+
 
 @app.route('/quote/random/', methods=['GET'])
 def get_random_quote():
   random_quote = choice(quotes)
-  random_quotes_id = random_quote["id"]
-  return random_quote
+  random_quote_id = random_quote["id"]
+  quote_author_name = get_quote_author(random_quote_id)
+
+  return {
+    "quoteId": random_quote_id,
+    "quote": random_quote["quote"],
+    "author": quote_author_name
+  } , 200
